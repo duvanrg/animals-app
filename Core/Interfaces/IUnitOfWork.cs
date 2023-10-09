c160d3372b83bc7db04bb7a7cd35888cc0ee9200
duvanrg <135860225+duvanrg@users.noreply.github.com>
2023-10-09 12:35:24 -0500

    feat: :sparkles: add references
    references for interfaces and repos in unitofwork
    
    @duvanrg

--- Core/Interfaces/IUnitOfWork.cs
+++ Core/Interfaces/IUnitOfWork.cs
@@ -2,8 +2,16 @@ namespace Core.Interfaces
 {
     public interface IUnitOfWork
     {
+        ICita Citas { get; }
+        ICiudad Ciudades { get; }
+        ICliente Clientes { get; }
+        IClienteDireccion ClienteDirecciones { get; }
+        IClienteTelefono ClienteTelefonos { get; }
+        IDepartamento Departamentos { get; }
+        IMascota Mascotas { get; }
         IPais Paises { get; }
-        
-        Task<int> SaveAsync(); 
+        IRaza Razas { get; }
+        IServicio Servicios { get; }
+        Task<int> SaveAsync();
     }
 }
--- Infrastructure/UnitOfWork/UnitOfWork.cs
+++ Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities;
 using Core.Interfaces;
 using Infrastructure.Data.Configuration;
 using Infrastructure.Repositories;
@@ -11,7 +12,16 @@ namespace Infrastructure.UnitOfWork;
 public class UnitOfWork : IUnitOfWork, IDisposable
 {
     private readonly AnimalContext _context;
+    private CitaRepo _Citas;
+    private CiudadRepo _Ciudades;
+    private ClienteRepo _Clientes;
+    private ClienteDireccionRepo _ClienteDirecciones;
+    private ClienteTelefonoRepo _ClienteTelefonos;
+    private DepartamentoRepo _Departamentos;
+    private MascotaRepo _Mascotas;
     private PaisRepo _paises;
+    private RazaRepo _Razas;
+    private ServicioRepo _Servicios;
 
     public UnitOfWork(AnimalContext context)
     {
@@ -19,6 +29,83 @@ public class UnitOfWork : IUnitOfWork, IDisposable
 
     }
 
+    public ICita Citas
+    {
+        get
+        {
+            if (_Citas == null)
+            {
+                _Citas = new CitaRepo(_context);
+            }
+            return _Citas;
+        }
+    }
+    public ICiudad Ciudades
+    {
+        get
+        {
+            if (_Ciudades == null)
+            {
+                _Ciudades = new CiudadRepo(_context);
+            }
+            return _Ciudades;
+        }
+    }
+    public ICliente Clientes
+    {
+        get
+        {
+            if (_Clientes == null)
+            {
+                _Clientes = new ClienteRepo(_context);
+            }
+            return _Clientes;
+        }
+    }
+    public IClienteDireccion ClienteDirecciones
+    {
+        get
+        {
+            if (_ClienteDirecciones == null)
+            {
+                _ClienteDirecciones = new ClienteDireccionRepo(_context);
+            }
+            return _ClienteDirecciones;
+        }
+    }
+    public IClienteTelefono ClienteTelefonos
+    {
+        get
+        {
+            if (_ClienteTelefonos == null)
+            {
+                _ClienteTelefonos = new ClienteTelefonoRepo(_context);
+            }
+            return _ClienteTelefonos;
+        }
+    }
+    public IDepartamento Departamentos
+    {
+        get
+        {
+            if (_Departamentos == null)
+            {
+                _Departamentos = new DepartamentoRepo(_context);
+            }
+            return _Departamentos;
+        }
+    }
+    public IMascota Mascotas
+    {
+        get
+        {
+            if (_Mascotas == null)
+            {
+                _Mascotas = new MascotaRepo(_context);
+            }
+            return _Mascotas;
+        }
+    }
     public IPais Paises
     {
         get
@@ -28,6 +115,28 @@ public class UnitOfWork : IUnitOfWork, IDisposable
                 _paises = new PaisRepo(_context);
             }
             return _paises;
+        }
+    }
+    public IRaza Razas
+    {
+        get
+        {
+            if (_Razas == null)
+            {
+                _Razas = new RazaRepo(_context);
+            }
+            return _Razas;
+        }
+    }
+    public IServicio Servicios
+    {
+        get
+        {
+            if (_Servicios == null)
+            {
+                _Servicios = new ServicioRepo(_context);
+            }
+            return _Servicios;
         }
     }
 
